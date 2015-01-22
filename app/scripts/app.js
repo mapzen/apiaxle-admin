04de1726012851b9bb59eab3afaebc2e0b772cc1
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2015-01-22 17:28:35 -0500

    cleanup cruft

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,10 +25,6 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
       .when('/apis', {
         templateUrl: 'views/apis.html',
         controller: 'ApisCtrl'
